2bacb641c4ed03fc8ea0c8c41290038d10004e29
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-02-18 22:52:46 +0100

    feat: ⭐️ add styling improvements

--- src/js/cookie-bar.js
+++ src/js/cookie-bar.js
@@ -1,10 +1,11 @@
 const cookieValue = document.cookie.match(/(;)?cookiebar=([^;]*);?/) ? document.cookie.match(/(;)?cookiebar=([^;]*);?/)[2] : 'CokkiesUnset';
-console.log(cookieValue)
+
 if (cookieValue === 'CookieAllowed') {
     const thumbnail = document.getElementsByClassName('trailer__thumbnail')[0];
     const playerContainer = document.getElementsByClassName('trailer__container')[0];
-    const iframe = `<iframe id="iframe" class="trailer__player" width="560" 
-                        height="315" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
+    const thumbnailPlaceholder = document.getElementsByClassName('trailer__thumbnail')[0].getBoundingClientRect();
+    const iframe = `<iframe id="iframe" class="trailer__player" width="${thumbnailPlaceholder.width}" 
+                        height="${thumbnailPlaceholder.height}" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
                         clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
     thumbnail.classList.add('trailer__thumbnail--hide');
