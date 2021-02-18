f724c219c990a65830cbc07bc89556d4a9164656
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-02-18 16:00:28 +0100

    feat: ⭐️ upgrade webpack config and boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -72,28 +72,28 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
     }),
-    new ImageMinimizerPlugin({
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      minimizerOptions: {
-        // Lossless optimization with custom option
-        // Feel free to experiment with options for better result for you
-        plugins: [
-          ['gifsicle', { interlaced: true }],
-          ['jpegtran', { progressive: true }],
-          ['optipng', { optimizationLevel: 5 }],
-          [
-            'svgo',
-            {
-              plugins: [
-                {
-                  removeViewBox: false,
-                },
-              ],
-            },
-          ],
-        ],
-      },
-    }),
+    // new ImageMinimizerPlugin({
+    //   test: /\.(jpe?g|png|gif|svg)$/i,
+    //   minimizerOptions: {
+    //     // Lossless optimization with custom option
+    //     // Feel free to experiment with options for better result for you
+    //     plugins: [
+    //       ['gifsicle', { interlaced: true }],
+    //       ['jpegtran', { progressive: true }],
+    //       ['optipng', { optimizationLevel: 5 }],
+    //       [
+    //         'svgo',
+    //         {
+    //           plugins: [
+    //             {
+    //               removeViewBox: false,
+    //             },
+    //           ],
+    //         },
+    //       ],
+    //     ],
+    //   },
+    // }),
     new CleanWebpackPlugin({
       verbose: true,
       cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
