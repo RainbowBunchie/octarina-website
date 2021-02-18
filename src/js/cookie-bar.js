5340013ee7c452b5e5f1e0572086612189f6d4a9
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-02-18 23:50:25 +0100

    fix: 🐛 add width to player that is independent of thumbnail

--- src/js/cookie-bar.js
+++ src/js/cookie-bar.js
@@ -3,9 +3,9 @@ const cookieValue = document.cookie.match(/(;)?cookiebar=([^;]*);?/) ? document.
 if (cookieValue === 'CookieAllowed') {
     const thumbnail = document.getElementsByClassName('trailer__thumbnail')[0];
     const playerContainer = document.getElementsByClassName('trailer__container')[0];
-    const thumbnailPlaceholder = document.getElementsByClassName('trailer__thumbnail')[0].getBoundingClientRect();
-    const iframe = `<iframe id="iframe" class="trailer__player" width="${thumbnailPlaceholder.width}" 
-                        height="${thumbnailPlaceholder.height}" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
+    const wrapper = document.getElementsByClassName('trailer')[0].getElementsByClassName('wrapper')[0].getBoundingClientRect();
+    const iframe = `<iframe id="iframe" class="trailer__player" width="${wrapper.width}" 
+                        height="${wrapper.width * 0.563}" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
                         clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
     thumbnail.classList.add('trailer__thumbnail--hide');
