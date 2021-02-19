6af324a6dd0eac0f365b3bd852115ad96240fbb3
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-02-19 11:45:25 +0100

    fix: 🐛 fix width of youtube video to fit wrapper

--- src/js/cookie-bar.js
+++ src/js/cookie-bar.js
@@ -4,9 +4,9 @@ if (cookieValue === 'CookieAllowed') {
     console.log('test')
     const thumbnail = document.getElementsByClassName('trailer__thumbnail')[0];
     const playerContainer = document.getElementsByClassName('trailer__container')[0];
-    const wrapper = document.getElementsByClassName('trailer')[0].getElementsByClassName('wrapper')[0].getBoundingClientRect();
-    const iframe = `<iframe id="iframe" class="trailer__player" width="${wrapper.width}" 
-                        height="${wrapper.width * 0.563}" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
+    const width = window.innerWidth < 1000 ? window.innerWidth - 72 : 1000 - 72;
+    const iframe = `<iframe id="iframe" class="trailer__player" width="${width}" 
+                        height="${width * 0.563}" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
                         clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
     thumbnail.classList.add('trailer__thumbnail--hide');
