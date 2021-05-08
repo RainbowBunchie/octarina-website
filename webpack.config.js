cd02af397088ccb263e6ddeb9086e0b0d4aaa18b
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-05-08 10:15:35 +0200

    feat: ⭐️ add ImageMinimizerPlugin to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
@@ -92,6 +92,21 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'css/[name].css',
+        }),
+        new ImageMinimizerPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            minimizerOptions: {
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 5 }],
+                    ['svgo', {
+                        plugins: [{
+                            removeViewBox: false,
+                        }],
+                    }],
+                ],
+            },
         }),
         new CleanWebpackPlugin({
             verbose: true,
