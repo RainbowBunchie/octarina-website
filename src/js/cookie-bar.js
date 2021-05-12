322a494fa345c0b0afd6cf1de3f04ca780b01cfb
Denise Buder <denisebuder@Denises-MacBook-Pro-2.local>
2021-05-12 21:35:18 +0200

    feat: ⭐️ add images and videos to press page, refactor cookiebar

--- src/js/cookie-bar.js
+++ src/js/cookie-bar.js
@@ -1,13 +1,16 @@
 const cookieValue = document.cookie.match(/(;)?cookiebar=([^;]*);?/) ? document.cookie.match(/(;)?cookiebar=([^;]*);?/)[2] : 'CokkiesUnset';
 
 if (cookieValue === 'CookieAllowed') {
-    const thumbnail = document.getElementsByClassName('trailer__thumbnail')[0];
-    const playerContainer = document.getElementsByClassName('trailer__container')[0];
-    const width = window.innerWidth < 1000 ? window.innerWidth - 72 : 1000 - 72;
-    const iframe = `<iframe id="iframe" class="trailer__player" width="${width}" 
-                        height="${width * 0.563}" src="https://www.youtube.com/embed/w9DKBKqvu4g" frameborder="0" allow="accelerometer; autoplay; 
-                        clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    const videos = document.querySelectorAll('.youtube');
+    for (const video of videos) {
+        const thumbnail = video.querySelector('.thumbnail');
+        const width = window.innerWidth < 1000 ? window.innerWidth - 72 : 1000 - 72;
+        const youtube = thumbnail.getAttribute('data-youtube');
+        const iframe = `<iframe id="iframe" class="player" width="${width}" 
+                            height="${width * 0.563}" src="${youtube}" frameborder="0" allow="accelerometer; 
+                            clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
-    thumbnail.classList.add('trailer__thumbnail--hide');
-    playerContainer.innerHTML = iframe;
+        video.innerHTML = iframe;
+        thumbnail.classList.add('thumbnail--hide');
+    }
 }
